6fd0f018342a462323c68cfea116ec6a0b770e7e
Greg Jaskiewicz <gryzman@gmail.com>
2013-06-22 12:29:47 +0100

    assume radians, no need for conversion from deg to radians. This isn't this function's task

--- angle_difference.c
+++ angle_difference.c
@@ -5,12 +5,10 @@
 #define MIN_ABS(x,y) ({ __typeof(x) __a = x; __typeof(y) __b = y; fabs(__a) < fabs(__b) ? __a : __b; })
 #define MIN3_ABS(X, Y, Z) ( min_abs(min_abs(X, Y), Z) )
 
-double angle_difference(double neworientation, double oldorientation)
-{
-  double newrotation = neworientation*(M_PI/180.0);
-  double oldrotation = oldorientation*(M_PI/180.0);
 
-  double by = MIN3_ABS(newrotation-oldrotation, newrotation-oldrotation+2*M_PI, newrotation-oldrotation-2*M_PI);
+inline double angle_difference(double neworientation, double oldorientation)
+{
+  double by = MIN3_ABS(neworientation-oldorientation, neworientation-oldorientation+2*M_PI, neworientation-oldorientation-2*M_PI);
 
   return by;
 }
