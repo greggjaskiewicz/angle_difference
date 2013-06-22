cb87586ce463f7510cefa78508ab6cbec661314b
Greg Jaskiewicz <gryzman@gmail.com>
2013-06-22 08:56:58 +0100

    few changes following suggestions on irc #iphonedev

--- angle_difference.c
+++ angle_difference.c
@@ -2,15 +2,15 @@
 // share and preach the love
 // public domain, (c) Greg Jaskiewicz 2013
 
-#define min_abs(X, Y) ( fabs(X) < fabs(Y) ? (X) : (Y) )
-#define min3_abs(X, Y, Z) ( min_abs(min_abs(X, Y), Z) )
+#define MIN_ABS(x,y) ({ __typeof(x) __a = x; __typeof(y) __b = y; fabs(__a) < fabs(__b) ? __a : __b; })
+#define MIN3_ABS(X, Y, Z) ( min_abs(min_abs(X, Y), Z) )
 
 double angle_difference(double neworientation, double oldorientation)
 {
   double newrotation = neworientation*(M_PI/180.0);
   double oldrotation = oldorientation*(M_PI/180.0);
 
-  double by = min3_abs(newrotation-oldrotation, newrotation-oldrotation+2*M_PI, newrotation-oldrotation-2*M_PI);
+  double by = MIN3_ABS(newrotation-oldrotation, newrotation-oldrotation+2*M_PI, newrotation-oldrotation-2*M_PI);
 
   return by;
 }
